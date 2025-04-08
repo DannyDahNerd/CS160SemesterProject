c623ea7187d9fa458ba9a4b91053d6582bc20ce7
Daniel Ly <68573397+DannyDahNerd@users.noreply.github.com>
2025-04-08 16:11:27 -0700

    1:28:50
    Right before shadcn toast is added

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -1,8 +1,13 @@
 import {Client, Account, Databases, Storage, Avatars } from 'appwrite';
 
 export const appwriteConfig = {
-    projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID || '67cf8bc0002ce40cf9ba',
+    projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID || '67f501f2003703c8ad09',
     url: import.meta.env.VITE_APPWRITE_PROJECT_URL || 'https://cloud.appwrite.io/v1',
+    storageId: import.meta.env.VITE_APPWRITE_STORAGE_ID || '67f5a0640027b7160acd',
+    databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID || '67f5a0a400270962556e',
+    savesCollectionId: import.meta.env.VITE_APPWRITE_SAVES_COLLECTION_ID || '67f5a2f5002535a425ac',
+    userCollectionId: import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID || '67f5a2e10028e82a3b66',
+    postCollectionId: import.meta.env.VITE_APPWRITE_POST_COLLECTION_ID || '67f5a13b00157b01b064',
 }
 
 export const client = new Client();
